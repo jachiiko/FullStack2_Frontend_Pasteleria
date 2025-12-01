99996d036ed51660acf01a58fb5ffd211df662a8
Diego Ignacio Herrera Venegas <147668937+DiegoHerrera22@users.noreply.github.com>
2025-12-01 08:40:29 -0300

    Update ProductCard.jsx

--- src/molecules/ProductCard.jsx
+++ src/molecules/ProductCard.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Button from '../atoms/Button.jsx';
 
-/**
- * Tarjeta de producto que muestra imagen, categoría, nombre, precio y un botón para añadir al carrito.
- */
 export default function ProductCard({ product, onAdd }) {
-  const { image, name, category, price } = product;
+  const { imageUrl, imageurl, name, category, price } = product;
+
   return (
     <div className="product-card">
-      <img src={image} alt={name} />
+      <img
+        src={imageUrl || imageurl}
+        alt={name}
+        style={{ objectFit: 'cover' }}
+      />
+
       <div className="product-card-content">
         {category && <div className="product-card-category">{category}</div>}
         <div className="product-card-name">{name}</div>
         <div className="product-card-price">${price.toLocaleString()}</div>
+
         <Button className="add-btn" onClick={() => onAdd(product)}>
           Añadir
         </Button>
